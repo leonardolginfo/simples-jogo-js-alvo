858d0a3005e01aac19c03f7ad6cb8476e0114fcd
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-01-18 11:10:31 -0300

    index e js criadas, funcoes basicas ok

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function desenhaCirculo(x, y, raio, cor) {
+    pincel.fillStyle = cor;
+    pincel.beginPath();
+    pincel.arc(x, y, raio, 0, 2 * Math.PI);
+    pincel.fill();
+}
+
+function limpaTela() {
+    pincel.clearRect(0, 0, 800, 600);
+}
